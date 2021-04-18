6b38200707f8977d812b32defd3b24006dd8d1f3
Hector Chicas <hect0rchicas@hotmail.com>
2021-04-18 15:49:22 -0400

    Initial commit

---
+++ app/src/main/java/com/hfad/workout/Workout.java
@@ -0,0 +1,44 @@
+package com.hfad.workout;
+
+public class Workout {
+    private String name;
+    private String description;
+
+    public static final Workout[] workouts = {
+            new Workout("The Limb Loosener",
+                    "5 Handstand push-ups\n10 1-legged squats\n15 Pull-ups"),
+            new Workout("Core Agony",
+                    "100 Pull-ups\n100 Push-ups\n100 Sit-ups\n100 Squats"),
+            new Workout("The Wimp Special",
+                    "5 Pull-ups\n10 Push-ups\n15 Squats"),
+            new Workout("Strength and Length",
+                    "500 meter run\n21 x 1.5 pood kettleball swing\n21 x pull-ups")
+    };
+
+    //each workout has a name and description
+    private Workout(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String[] getNamesArray() {
+        String[] names = new String[Workout.workouts.length];
+
+        for (int i = 0; i < names.length; i++) {
+            names[i] = Workout.workouts[i].getName();
+        }
+        return names;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
